d527dea7992be6d21d6159f54adab027777d7dd1
EmmyTexx <67845699+EmmyTexx@users.noreply.github.com>
2020-07-05 01:12:41 +0100

    Update __main__.py

--- tobrot/__main__.py
+++ tobrot/__main__.py
@@ -62,25 +62,25 @@ if __name__ == "__main__" :
     #
     # incoming_message_handler = MessageHandler(
     #     incoming_message_f,
-    #     filters=Filters.command(["leech"]) & Filters.chat(chats=AUTH_CHANNEL)
+    #     filters=Filters.command(["leech@BuhariBotBot"]) & Filters.chat(chats=AUTH_CHANNEL)
     # )
     # app.add_handler(incoming_message_handler)
     #
     incoming_purge_message_handler = MessageHandler(
         incoming_purge_message_f,
-        filters=Filters.command(["purge"]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=Filters.command(["purge@BuhariBotBot"]) & Filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(incoming_purge_message_handler)
     #
     # incoming_youtube_dl_handler = MessageHandler(
     #     incoming_youtube_dl_f,
-    #     filters=Filters.command(["ytdl"]) & Filters.chat(chats=AUTH_CHANNEL)
+    #     filters=Filters.command(["ytdl@BuhariBotBot"]) & Filters.chat(chats=AUTH_CHANNEL)
     # )
     # app.add_handler(incoming_youtube_dl_handler)
     #
     status_message_handler = MessageHandler(
         status_message_f,
-        filters=Filters.command(["status"]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=Filters.command(["status@BuhariBotBot"]) & Filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(status_message_handler)
     #
@@ -98,19 +98,19 @@ if __name__ == "__main__" :
     #
     rename_message_handler = MessageHandler(
         rename_message_f,
-        filters=Filters.command(["rename"]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=Filters.command(["rename@BuhariBotBot"]) & Filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(rename_message_handler)
     #
     upload_document_handler = MessageHandler(
         upload_document_f,
-        filters=Filters.command(["upload"]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=Filters.command(["upload@BuhariBotBot"]) & Filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(upload_document_handler)
 
     help_text_handler = MessageHandler(
         help_message_f,
-        filters=Filters.command(["help"]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=Filters.command(["help@BuhariBotBot"]) & Filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(help_text_handler)
     #
@@ -133,13 +133,13 @@ if __name__ == "__main__" :
     #
     save_thumb_nail_handler = MessageHandler(
         save_thumb_nail,
-        filters=Filters.command(["savethumbnail"]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=Filters.command(["savethumbnail@BuhariBotBot"]) & Filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(save_thumb_nail_handler)
     #
     clear_thumb_nail_handler = MessageHandler(
         clear_thumb_nail,
-        filters=Filters.command(["clearthumbnail"]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=Filters.command(["clearthumbnail@BuhariBotBot"]) & Filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(clear_thumb_nail_handler)
     # 
